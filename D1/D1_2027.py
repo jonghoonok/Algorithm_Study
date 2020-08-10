c575a73b8191ededdce352d5d2e04776c665c422
jonghoonok <jonghoone94@gmail.com>
2020-08-10 18:34:40 +0900

    D1 problem solving

---
+++ D1/D1_2027.py
@@ -0,0 +1,5 @@
+plus = '++++'
+sharp = '#'
+
+for i in range(5):
+    print(plus[:i]+sharp+plus[i:])
---
+++ D1/D1_2043.py
@@ -0,0 +1,5 @@
+a, b = map(int, input().split())
+if a>b:
+    print(a-b+1)
+else:
+    print(1000+a-b+1)
